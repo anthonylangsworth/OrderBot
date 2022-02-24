274b6cf752f86548ec09b5de4ca95eaafc616388
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-02-24 22:13:46 +1100

    Initial commit

---
+++ MinorFactionMonitor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ionic.Zlib;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Text;
+
+using SubscriberSocket client = new SubscriberSocket();
+client.Options.ReceiveHighWatermark = 1000;
+client.Connect("tcp://eddn.edcd.io:9500");
+client.SubscribeToAnyTopic();
+
+while (true)
+{
+    if (client.TryReceiveFrameBytes(out byte[]? compressed, out bool more))
+    {
+        byte[] uncompressed = ZlibStream.UncompressBuffer(compressed);
+        string result = Encoding.UTF8.GetString(uncompressed);
+        Console.WriteLine(result);
+    }
+}
