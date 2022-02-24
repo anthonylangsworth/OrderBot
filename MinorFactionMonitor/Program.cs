81f754a32cd5b173e7de13c1967c41fc7247cdd5
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-02-25 00:22:09 +1100

    Use JsonDocument

--- MinorFactionMonitor/Program.cs
+++ MinorFactionMonitor/Program.cs
@@ -2,6 +2,7 @@
 using NetMQ;
 using NetMQ.Sockets;
 using System.Text;
+using System.Text.Json;
 
 using SubscriberSocket client = new SubscriberSocket();
 client.Options.ReceiveHighWatermark = 1000;
@@ -14,6 +15,35 @@ while (true)
     {
         byte[] uncompressed = ZlibStream.UncompressBuffer(compressed);
         string result = Encoding.UTF8.GetString(uncompressed);
+
+        JsonDocument document = JsonDocument.Parse(result);
+
+        // Get UTC time of last update
+        DateTime timestamp = document.RootElement
+                .GetProperty("header")
+                .GetProperty("gatewayTimestamp")
+                .GetDateTime();
+
+        JsonElement messageElement = document.RootElement.GetProperty("message");
+        string? starSystemName = null;
+        if (messageElement.TryGetProperty("StarSystem", out JsonElement starSystemProperty))
+        {
+            starSystemName = starSystemProperty.GetString();
+        }
+        if (starSystemName != null && messageElement.TryGetProperty("Factions", out JsonElement factionsProperty))
+        {
+            factionsProperty.EnumerateArray().First(element => element.GetProperty("Name").GetString().Equals()
+        }
+
         Console.WriteLine(result);
     }
 }
+
+record MinorFactionInfo
+{
+    string Name;
+    double Influence;
+    string[] States;
+}
+
+public 
