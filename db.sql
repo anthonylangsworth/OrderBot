b3d56446b9f4ab1631c9131bcb0d7e82ed58f932
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-02-28 21:15:41 +1100

    Script more database objects

--- db.sql
+++ db.sql
@@ -1,4 +1,19 @@
-﻿USE [OrderBot]
+﻿CREATE DATABASE [OrderBot]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'OrderBot', FILENAME = N'/var/opt/mssql/data/OrderBot.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'OrderBot_log', FILENAME = N'/var/opt/mssql/data/OrderBot_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [OrderBot].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+USE [OrderBot]
 GO
 
 SET ANSI_NULLS ON
@@ -38,3 +53,18 @@ CREATE TABLE [dbo].[SystemMinorFactionState](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+USE [master]
+GO
+
+CREATE LOGIN [OrderBot] WITH PASSWORD=N'<password>', DEFAULT_DATABASE=[OrderBot], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+USE [OrderBot]
+GO
+
+CREATE USER [OrderBot] FOR LOGIN [OrderBot] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+
+
