992cb220760f076b63a5f247a371519ddf9766a5
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-03-08 16:21:47 +1100

    Expand test queries

--- db/test.sql
+++ db/test.sql
@@ -1,5 +1,21 @@
-﻿select * from StarSystemMinorFaction
-select * from StarSystemMinorFactionState
+﻿-- Influence for each minor faction in each system
+select ss.Name as [Star System], mf.Name as [Minor Faction], ssmf.Influence
+from StarSystemMinorFaction ssmf left join StarSystem ss
+	on ssmf.StarSystemId = ss.Id
+left join MinorFaction mf
+	on ssmf.MinorFactionId = mf.Id
+
+-- System state
+select ss.Name as [Star System], mf.Name as [Minor Faction], State.Name as [State]
+from StarSystemMinorFactionState ssmfs left join StarSystemMinorFaction ssmf
+	on ssmfs.StarSystemMinorFactionsId = ssmf.Id
+left join StarSystem ss
+	on ssmf.StarSystemId = ss.Id
+left join MinorFaction mf
+	on ssmf.MinorFactionId = mf.Id
+left join State 
+	on ssmfs.StatesId = State.Id
+
 select * from MinorFaction
 select * from StarSystem
 select * from State
