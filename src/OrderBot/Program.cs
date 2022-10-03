185877c04047bc9fcfe55ecc248a83d966c78af5
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-10-03 23:29:31 +1100

    Correct env var name

--- src/OrderBot/Program.cs
+++ src/OrderBot/Program.cs
@@ -17,7 +17,7 @@ IHost host = Host.CreateDefaultBuilder(args)
         if (string.IsNullOrEmpty(dbConnectionString))
         {
             throw new InvalidOperationException(
-                $"Database connection string missing from environment variable `{environmentVariablePrefix}ConnectionString__{databaseEnvironmentVariable}`. " +
+                $"Database connection string missing from environment variable `{environmentVariablePrefix}ConnectionStrings__{databaseEnvironmentVariable}`. " +
                 "Usually in the form of `Server=server;Database=OrderBot;User ID=OrderBot;Password=password`.");
         }
         services.AddDbContextFactory<OrderBotDbContext>(
