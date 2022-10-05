1b774321cfa59506cab9d58539122ee80403d2f7
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-10-05 20:17:58 +1100

    Clean up

--- src/OrderBot/Reports/Goals.cs
+++ src/OrderBot/Reports/Goals.cs
@@ -4,25 +4,15 @@
     {
         // public static Goal Ignore => new Goal("Ignore", "Generate no orders for this system. Useful systems that you do not care about.");
 
-        public static Goal Control => ControlGoal.Instance;
-
         // public static Goal Maintain => new Goal("Control", "Neither retreat nor control. Aim to keep influence up to 10% less than the controlling minor faction.");
 
         // public static Goal Retreat => new Goal("Retreat", "Retreat from the system by reducing influence below 5% and keeping it there.");
 
-        public static Goal Default => Control;
-
-        public static Goal[] All => new[]
-        {
-            // Ignore,
-            Control,
-            // Maintain,
-            // Retreat
-        };
+        public static Goal Default => ControlGoal.Instance;
 
         public static IDictionary<string, Goal> Map = new Dictionary<string, Goal>
         {
-            { Control.Name, Control }
+            { ControlGoal.Instance.Name, ControlGoal.Instance }
         };
     }
 }
