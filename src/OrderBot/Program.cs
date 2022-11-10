11d62e6817731129e4763adfeee6c9fd898468c3
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-11-11 01:08:46 +1100

    Log to Log Analytics directly

--- src/OrderBot/Program.cs
+++ src/OrderBot/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using OrderBot.Admin;
 using OrderBot.CarrierMovement;
 using OrderBot.Discord;
@@ -15,6 +17,9 @@ internal class Program
                          .ConfigureHostConfiguration(configurationBuilder => configurationBuilder.AddEnvironmentVariables())
                          .ConfigureServices((hostContext, services) =>
                          {
+                             services.AddLogging(builder => builder.AddLogAnalytics(
+                                 hostContext.Configuration.GetRequiredSection("LogAnalytics_WorkspaceId").Value ?? "",
+                                 hostContext.Configuration.GetRequiredSection("LogAnalytics_WorkspaceKey").Value ?? ""));
                              services.AddDatabase(hostContext.Configuration);
                              services.AddEddnMessageProcessor();
                              services.AddTodoList();
