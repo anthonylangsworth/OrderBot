c9307869e9824305d8da48a81aa92cdadbb6f3e5
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-11-27 14:15:49 +1100

    Expand comment

--- src/OrderBot/CarrierMovement/Signal.cs
+++ src/OrderBot/CarrierMovement/Signal.cs
@@ -3,9 +3,17 @@
 namespace OrderBot.CarrierMovement;
 
 /// <summary>
+/// A signal source in the EDDN message schema.
+/// </summary>
+/// <remarks>
+/// <para>
+/// Used by <see cref="CarrierMovementMessageProcessor"/> to parse EDDN message JSON.
+/// </para>
+/// <para>
 /// Based off https://github.com/EDCD/EDDN/blob/master/schemas/fsssignaldiscovered-v1.0.json for the schema
 /// "signals": [{"IsStation": true, "SignalName": "THE PEAKY BLINDERS KNF-83G", "timestamp": "2022-10-13T12:13:09Z"}]
-/// </summary>
+/// </para>
+/// </remarks>
 internal record Signal
 {
     [JsonInclude]
