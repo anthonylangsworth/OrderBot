00526977a7335c9551bb557e61c275c2b4f8e443
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-11-27 19:12:37 +1100

    Add potential way to invalidate caches

---
+++ src/OrderBot/MessageProcessors/MessageProcessorCacheInvalidator.cs
@@ -0,0 +1,17 @@
+﻿namespace OrderBot.MessageProcessors;
+
+/// <summary>
+/// Future idea for a cache system for <see cref="MessageProcessorCache"/> classes. TODO: Finish
+/// </summary>
+internal class MessageProcessorCacheInvalidator
+{
+    public MessageProcessorCacheInvalidator(IEnumerable<MessageProcessorCache> messageProcessors)
+    {
+
+    }
+
+    public void Invalidate(IEnumerable<string> dataTypes)
+    {
+
+    }
+}
