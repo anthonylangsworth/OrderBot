2ca68fc98e0f7ff2c416ca42a1d296ec6bf7a628
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-11-28 00:14:50 +1100

    Add missing caches

--- src/OrderBot/ToDo/ToDoListExtensions.cs
+++ src/OrderBot/ToDo/ToDoListExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OrderBot.CarrierMovement;
 using OrderBot.MessageProcessors;
 
 namespace OrderBot.ToDo;
@@ -7,6 +8,8 @@ internal static class ToDoListExtensions
 {
     internal static void AddTodoList(this IServiceCollection services)
     {
+        services.AddSingleton<SupportedMinorFactionsCache>();
+        services.AddSingleton<GoalStarSystemsCache>();
         services.AddSingleton<ToDoListApiFactory>();
         services.AddScoped<EddnMessageProcessor, ToDoListMessageProcessor>();
     }
