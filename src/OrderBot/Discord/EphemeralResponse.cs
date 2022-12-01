96358a7ec82544692fdc9600588f67cf78ce00e8
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-12-01 15:29:44 +1100

    Flesh out

--- src/OrderBot/Discord/EphemeralResponse.cs
+++ src/OrderBot/Discord/EphemeralResponse.cs
@@ -6,34 +6,61 @@ using System.Globalization;
 namespace OrderBot.Discord;
 
 /// <summary>
-/// Used by <see cref="BotHostedService"/> and command modules
-/// to indicate success or failure clearly to the user.
+/// Provide standard user responses to slash (Application) commands
+/// used with a <see cref="SocketInteractionContext"/>.
 /// </summary>
+/// <seealso cref="BotHostedService"/>
 internal class EphemeralResponse
 {
-    public readonly static string ErrorPrefix = "**Error**: ";
-    public readonly static string SuccessPrefix = "**Success**: ";
-
+    /// <summary>
+    /// Create a new <see cref="EphemeralResponse"/>.
+    /// </summary>
+    /// <param name="context">
+    /// The interaction to respond to.
+    /// </param>
     public EphemeralResponse(SocketInteractionContext context)
     {
-        context.Interaction.DeferAsync(ephemeral: true).GetAwaiter().GetResult();
         Context = context;
+        Context.Interaction.DeferAsync(ephemeral: true).GetAwaiter().GetResult();
     }
 
+    /// <summary>
+    /// The context to respond to.
+    /// </summary>
     protected SocketInteractionContext Context { get; }
+    protected readonly static string ErrorPrefix = "**Error**: ";
+    protected readonly static string SuccessPrefix = "**Success**: ";
 
+    /// <summary>
+    /// Inform the user of a successful change.
+    /// </summary>
+    /// <param name="message">
+    /// The message. It should describe what changed, referencing
+    /// relevant parameters.
+    /// </param>
+    /// <seealso cref="Error"/>
+    /// <seealso cref="Information"/>
     public async Task Success(string message)
     {
         await Write(SuccessPrefix, message);
     }
 
+    /// <summary>
+    /// Send a file to the user.
+    /// </summary>
+    /// <param name="file">
+    /// The file contents.
+    /// </param>
+    /// <param name="fileName">
+    /// The file name.
+    /// </param>
     public async Task File(string file, string fileName)
     {
         using MemoryStream memoryStream = new();
         using StreamWriter streamWriter = new(memoryStream);
         using CsvWriter csvWriter = new(streamWriter, CultureInfo.InvariantCulture);
-        csvWriter.WriteRecords(file);
-        csvWriter.Flush();
+        await csvWriter.WriteRecordsAsync(file);
+        await csvWriter.FlushAsync();
         memoryStream.Seek(0, SeekOrigin.Begin);
         await Context.Interaction.FollowupWithFileAsync(
             fileStream: memoryStream,
@@ -42,17 +69,41 @@ internal class EphemeralResponse
         );
     }
 
-    public async Task Error(string message)
+    /// <summary>
+    /// Inform the user of an error.
+    /// </summary>
+    /// <remarks>
+    /// The gaol is to provide more informative and actionable error messages.
+    /// </remarks>
+    /// <param name="what">
+    /// Describe the error.
+    /// </param>
+    /// <param name="why">
+    /// Describe the actions the user took that caused the error or, if it is internal, say so.
+    /// </param>
+    /// <param name="state">
+    /// Describe the system state. For example, were any changes saved?
+    /// </param>
+    /// <param name="fix">
+    /// Describe how to fix the error, such as retrying with the correct arguments.
+    /// </param>
+    public async Task Error(string what, string why, string state, string fix)
     {
-        await Write(ErrorPrefix, message);
+        await Write(ErrorPrefix, $"{what} {why} {state} {fix}");
     }
 
+    /// <summary>
+    /// Respond to a request for information.
+    /// </summary>
+    /// <param name="message">
+    /// The information.
+    /// </param>
     public async Task Information(string message)
     {
         await Write(string.Empty, message);
     }
 
-    public async Task Write(string prefix, string message)
+    protected async Task Write(string prefix, string message)
     {
         await Context.Interaction.FollowupAsync(
                 text: Limit($"{prefix}{message}"),
