2a65aff6e606452d4c18864081afe3ef9eea2967
Anthony Langsworth <anthony.langsworth@gmail.com>
2023-11-15 00:23:03 +1100

    Add more test queries

--- deploy/test.sql
+++ deploy/test.sql
@@ -1,4 +1,6 @@
-﻿-- Influence for each minor faction in each system
+﻿-- Some useful queries for debugging or seeing how the bot is used.
+
+-- Influence for each minor faction in each system
 select ss.Name as [Star System], mf.Name as [Minor Faction], ssmf.Influence
 from StarSystemMinorFaction ssmf left join StarSystem ss
 	on ssmf.StarSystemId = ss.Id
@@ -18,27 +20,13 @@ left join State
 	on ssmfs.StatesId = State.Id
 go
 
-/* Basic Report */
+-- Basic Report
 select ss.Name as [Star System], mf.Name as [Minor Faction], ssmf.Influence
 from StarSystem ss left join StarSystemMinorFaction ssmf
 	on ss.Id = ssmf.StarSystemId
 	left join MinorFaction mf
 	on ssmf.MinorFactionId = mf.Id
 order by ss.Name, ssmf.Influence desc
-
-/*
-select * from MinorFaction
-select * from StarSystem
-select * from State
-*/
-
-/*
-delete from State
-delete from StarSystem
-delete from MinorFaction
-delete from StarSystemMinorFaction
-delete from StarSystemMinorFactionState
-*/
 
 -- Ignored carriers
 select dg.GuildId, c.SerialNumber, c.Name, ss.Name
@@ -52,9 +40,23 @@ from Conflict c left join StarSystem ss on c.StarSystemId = ss.Id
 left join MinorFaction mf1 on c.MinorFaction1Id = mf1.Id
 left join MinorFaction mf2 on c.MinorFaction2Id = mf2.Id
 
--- All Systems tracked for a minor faction
+-- All Systems tracked for a Canonn
 select mf.Name, ss.Name
 from MinorFaction mf
 left join Presence p on p.MinorFactionId = mf.Id
 left join StarSystem ss on p.StarSystemId = ss.Id
 where mf.Name = 'Canonn'
+
+-- Minor faction support for each guild
+select dg.Name as [Discord Guild], mf.Name as [Minor Faction]
+from DiscordGuildMinorFaction dgmf
+left outer join DiscordGuild dg on dgmf.DiscordGuildId = dg.Id
+left outer join MinorFaction mf on dgmf.MinorFactionId = mf.Id
+
+-- Goals by Guild
+select dg.Name, dgpg.Goal, dg.Name, mf.Name
+from DiscordGuildPresenceGoal dgpg
+left outer join DiscordGuild dg on dgpg.DiscordGuildId = dg.Id
+left outer join Presence p on dgpg.PresenceId = p.Id
+left outer join MinorFaction mf on p.MinorFactionId = mf.Id
+order by dg.Name, dgpg.Goal
